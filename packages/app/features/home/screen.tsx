efd6cdd09657b07359c311897a56008f4a5fe49b
Chen <chenxi9649@gmail.com>
2023-01-04 11:53:15 -0800

    frontend auth next js is working, need to get expo working

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -2,44 +2,61 @@ import { Anchor, Button, H1, Input, Paragraph, Separator, Sheet, XStack, YStack 
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { SignIn, useAuth } from '@clerk/nextjs'
 
 export function HomeScreen() {
+  const { signOut } = useAuth()
+
   const linkProps = useLink({
     href: '/user/nate',
   })
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
+      <SignedIn>
+        <YStack space="$4" maw={600}>
+          <H1 ta="center">Welcome to Tamagui.</H1>
+          <Paragraph ta="center">
+            Here's a basic starter to show navigating from one screen to another. This screen uses
+            the same code on Next.js and React Native.
+          </Paragraph>
 
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
-          <Anchor
-            color="$color12"
-            href="https://github.com/tamagui/tamagui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            give it a ⭐️
-          </Anchor>
-        </Paragraph>
-      </YStack>
+          <Separator />
+          <Paragraph ta="center">
+            Made by{' '}
+            <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
+              @natebirdman
+            </Anchor>
+            ,{' '}
+            <Anchor
+              color="$color12"
+              href="https://github.com/tamagui/tamagui"
+              target="_blank"
+              rel="noreferrer"
+            >
+              give it a ⭐️
+            </Anchor>
+          </Paragraph>
+        </YStack>
 
-      <XStack>
-        <Button {...linkProps}>Link to user</Button>
-      </XStack>
-
-      <SheetDemo />
+        <XStack>
+          <Button {...linkProps}>Link to user</Button>
+        </XStack>
+        <SheetDemo />
+        {/* sign out button */}
+        <Button
+          onPress={() => {
+            signOut()
+          }}
+        >
+          Sign Out
+        </Button>
+      </SignedIn>
+      <SignedOut>
+        {/* click here to sign in */}
+        <SignIn />
+      </SignedOut>
     </YStack>
   )
 }
--- packages/app/features/signin/screen.tsx
+++ packages/app/features/signin/screen.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { YStack } from '@my/ui'
+import { Paragraph, YStack } from '@my/ui'
+import { SignIn } from '@clerk/nextjs'
 
 export function SignInScreen() {
   return (
     <YStack f={1} jc="center" ai="center" space>
-      aslkdjalksjd
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </YStack>
   )
 }

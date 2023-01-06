45555f2c15e6a46b46fdd3c0d8c2a6ad5522af49
Chen <chenxi9649@gmail.com>
2023-01-06 11:16:06 -0800

    updated landing page

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,9 +1,10 @@
-import { Anchor, Button, H1, Paragraph, Separator, Sheet, XStack, YStack } from '@my/ui'
+import { Anchor, Button, H1, Paragraph, Separator, Sheet, Text, XStack, YStack } from '@my/ui'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState, useEffect } from 'react'
 import { useLink } from 'solito/link'
 import { trpc } from '../../utils/trpc'
 import { SignedIn, SignedOut, useAuth, useSignIn, useSignUp } from '../../utils/clerk'
+import { Card, CardHeader, H3, Image } from 'tamagui' // or @tamagui/card
 
 export function HomeScreen() {
   const { signOut, userId } = useAuth()
@@ -34,36 +35,65 @@ export function HomeScreen() {
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
       <YStack space="$4" maw={600}>
-        <H1 ta="center">Welcome to Tamagui.</H1>
-        <Paragraph ta="center">
-          Here's a basic starter to show navigating from one screen to another. This screen uses the
-          same code on Next.js and React Native.
-        </Paragraph>
-
-        <Separator />
-        <Paragraph ta="center">
-          Made by{' '}
-          <Anchor color="$color12" href="https://twitter.com/natebirdman" target="_blank">
-            @natebirdman
-          </Anchor>
-          ,{' '}
+        <H1 ta="center">create-universal-app</H1>
+        <Paragraph ta="left">
+          This is a demo for create-universal-app. To read more about the philosophy behind it,
+          visit{' '}
           <Anchor
             color="$color12"
-            href="https://github.com/tamagui/tamagui"
+            href="https://github.com/chen-rn/create-universal-app"
             target="_blank"
-            rel="noreferrer"
           >
-            give it a ⭐️
-          </Anchor>
+            https://github.com/chen-rn/create-universal-app
+          </Anchor>{' '}
+          and give it a ⭐️ on github!
         </Paragraph>
+        <Separator />
+        <Paragraph>
+          On a high level, this is a template designed for you to build a universal app(web +
+          mobile) in one code base without sacrificing 1. Performance and 2. Developer Experience.
+          It uses Expo, Next, Solito, tRPC, Tamagui, Clerk, and Prisma. If you're a beginner and is
+          a little overwhelmed, I've also made a{' '}
+          <Anchor
+            color="$color12"
+            href="https://www.youtube.com/watch?v=6Z4p-qjnKCQ"
+            target="_blank"
+          >
+            video
+          </Anchor>{' '}
+          explanation on how this template works and how to get started!
+        </Paragraph>
+        <Separator />
       </YStack>
-      {data?.map((entry) => (
-        <Paragraph key={entry.id}>{entry.id}</Paragraph>
-      ))}
 
-      <Paragraph>{userId}</Paragraph>
+      <H3 ta="center">Some Demos</H3>
+      <YStack p={'$2'}>
+        <Paragraph>TRPC Demo</Paragraph>
+        {data?.map((entry) => (
+          <Paragraph key={entry.id}>{entry.id}</Paragraph>
+        ))}
+      </YStack>
+
+      <XStack space>
+        <Paragraph>Routing Demo</Paragraph>
+        <Button {...userLinkProps} theme={'gray'}>
+          User Page
+        </Button>
+      </XStack>
+
+      <XStack space ai="center">
+        <Paragraph>Clerk/Auth Demo</Paragraph>
+        <YStack space>
+          <Button {...signInLinkProps} theme={'gray'}>
+            Sign In
+          </Button>
+          <Button {...signUpLinkProps} theme={'gray'}>
+            Sign Up
+          </Button>
+        </YStack>
+      </XStack>
+
       <SignedIn>
-        {/* sign out button */}
         <Button
           onPress={() => {
             signOut()
@@ -72,19 +102,6 @@ export function HomeScreen() {
           Sign Out
         </Button>
       </SignedIn>
-      <XStack>
-        <Button {...userLinkProps} theme={'red'} mr="$4">
-          Link to User
-        </Button>
-        <YStack>
-          <Button {...signInLinkProps} theme={'blue'}>
-            Link to Sign In
-          </Button>
-          <Button {...signUpLinkProps} theme={'gray'}>
-            Link to Sign Up
-          </Button>
-        </YStack>
-      </XStack>
       <SheetDemo />
     </YStack>
   )

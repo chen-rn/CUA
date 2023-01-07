65ecfaffc8228139cffcd2a533141c3cf6fa02fc
Chen <chenxi9649@gmail.com>
2023-01-06 16:09:24 -0800

    v1 complete

--- packages/app/provider/auth/index.tsx
+++ packages/app/provider/auth/index.tsx
@@ -1,12 +1,12 @@
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from './cache'
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "./cache";
 
-const clerk_frontend_api = 'clerk.blablablabla.lcl.dev'
+const clerk_frontend_api = "clerk.blablablabla.lcl.dev"; //ENTER YOUR ENV HERE
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider frontendApi={clerk_frontend_api} tokenCache={tokenCache}>
       {children}
     </ClerkProvider>
-  )
+  );
 }

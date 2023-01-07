0153cc725b75b5edab643230dcda7e6bd66ee7ea
Chen <chenxi9649@gmail.com>
2023-01-07 11:51:00 -0800

    changing heading title

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,20 +1,20 @@
-import '@tamagui/core/reset.css'
-import '@tamagui/font-inter/css/400.css'
-import '@tamagui/font-inter/css/700.css'
+import "@tamagui/core/reset.css";
+import "@tamagui/font-inter/css/400.css";
+import "@tamagui/font-inter/css/700.css";
 
-import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
-import { Provider } from 'app/provider'
-import Head from 'next/head'
-import React, { useMemo } from 'react'
-import type { SolitoAppProps } from 'solito'
-import 'raf/polyfill'
-import { trpc } from 'app/utils/trpc.web'
+import { NextThemeProvider, useRootTheme } from "@tamagui/next-theme";
+import { Provider } from "app/provider";
+import Head from "next/head";
+import React, { useMemo } from "react";
+import type { SolitoAppProps } from "solito";
+import "raf/polyfill";
+import { trpc } from "app/utils/trpc.web";
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
     <>
       <Head>
-        <title>Tamagui Example App</title>
+        <title>Universal Example</title>
         <meta name="description" content="Tamagui, Solito, Expo & Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -22,11 +22,11 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
 
 function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useRootTheme()
+  const [theme, setTheme] = useRootTheme();
 
   return (
     <NextThemeProvider onChangeTheme={setTheme}>
@@ -34,7 +34,7 @@ function ThemeProvider({ children }: { children: React.ReactNode }) {
         {children}
       </Provider>
     </NextThemeProvider>
-  )
+  );
 }
 
-export default trpc.withTRPC(MyApp)
+export default trpc.withTRPC(MyApp);

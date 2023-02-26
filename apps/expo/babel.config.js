92cbeed43b86032bd7bc86663d0a0ebc48d5ba9d
Kristian Gunder Kramås <kristiankramas@outlook.com>
2023-02-26 22:16:33 +0100

    Support .env in Expo

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -1,9 +1,23 @@
+const path = require("path");
+
 module.exports = function (api) {
+  const envPath = path.resolve(__dirname, `../../`, `.env`);
+  require("dotenv").config({ path: envPath });
   api.cache(true)
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
     plugins: [
       require.resolve("expo-router/babel"),
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: "../../.env",
+          allowlist: ["NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY"],
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
       [
         require.resolve('babel-plugin-module-resolver'),
         {

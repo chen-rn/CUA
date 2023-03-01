175680c49520af733396a0fbf2918346ec91e01c
Rogue Alexander <rotkosky.rogue@gmail.com>
2023-03-01 23:49:27 +0200

    Add getServerSideProps to the home screen in nextjs

--- apps/nextjs/pages/index.tsx
+++ apps/nextjs/pages/index.tsx
@@ -1,3 +1,13 @@
 import { HomeScreen } from 'app/features/home/screen'
+import { buildClerkProps, clerkClient, getAuth } from "@clerk/nextjs/server";
+
+// Use getServerSideProps to get the Clerk user
+// When rendering the page on the server, the Clerk user context will be available immediately in:
+//   <SignedIn, SignedOut, useAuth, useSignIn, useSignUp, useUser> imported from "app/utils/clerk"
+export const getServerSideProps = async ({ req }: any) => {
+  const { userId } = getAuth(req);
+  const user = userId ? await clerkClient.users.getUser(userId) : undefined;
+  return { props: { ...buildClerkProps(req, { user }) } };
+};
 
 export default HomeScreen
